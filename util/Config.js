92406f753c7d3febfe2248380b6499b62fcbf3ce
LHULYQ31 <lhy31lyq@126.com>
2019-04-19 18:08:47 +0800

    提交修改文件

--- util/Config.js
+++ util/Config.js
@@ -1,4 +1,4 @@
-const BaseUrl="http://121.196.201.35:8080/lyms/";
+const BaseUrl="http://192.168.0.40:8080/lyms/";
 export default Config={
     LOGIN:BaseUrl+"login/login",//登陆
     REGISTER:BaseUrl+"user/saveUser",//登陆注册接口
@@ -6,4 +6,9 @@ export default Config={
     VIEWS:BaseUrl+"viewSport/pageViewSpots",//加载景点
     PREVIEWIMAGE:BaseUrl+"attachment/previewImage",//预览图片
     VIEWSDETAIL:BaseUrl+"viewSport/getViewSpotById",//预览图片
+    GET_COLLECTIONS:BaseUrl+"collection/pageCollections",//查询我的景点的收藏
+    PLAN_DETAIL:BaseUrl+"plan/getPlanById",//查询路线的详情
+    SAVE_COLLECTION:BaseUrl+"collection/saveCollection",//收藏
+    GET_COMMENTS:BaseUrl+"comment/listComments",//查邮寄
+    SAVE_COMMENT:BaseUrl+"comment/saveComment",//保存邮寄
 };
